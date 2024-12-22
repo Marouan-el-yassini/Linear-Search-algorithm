6b9d912bb31dbffff1df2d878ae456f0e91e668b
Marouan El Yassini <186507586+Marouan-el-yassini@users.noreply.github.com>
2024-12-23 00:21:40 +0100

    Add files via upload

---
+++ LINEAR SEARCH.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linear_search_algorithm
+{
+    internal class Program
+    {
+        // Linearly search target in arr.
+        // If target is present, return the index;
+        // otherwise, return -1
+        public static int Search(int[] arr, int target)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == target)
+                    return i;
+            }
+            return -1;
+        }
+
+        public static void Main()
+        {
+            int[] arr = { 1, 10, 30, 15 };
+            int target = 3;
+            Console.WriteLine(Search(arr, target));
+        }
+    }
+}
+
